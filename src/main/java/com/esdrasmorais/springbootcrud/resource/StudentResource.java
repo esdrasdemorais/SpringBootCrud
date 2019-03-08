25ff295d2581b414d0b39ae6f43701b2650289e0
Esdras de Morais da Silva <esdrasdemorais@gmail.com>
2019-03-08 01:29:42 -0800

    Finished All CRUD Operations

--- src/main/java/com/esdrasmorais/springbootcrud/resource/StudentResource.java
+++ src/main/java/com/esdrasmorais/springbootcrud/resource/StudentResource.java
@@ -1,11 +1,21 @@
 package com.esdrasmorais.springbootcrud.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.esdrasmorais.springbootcrud.StudentNotFoundException;
 import com.esdrasmorais.springbootcrud.model.Student;
 import com.esdrasmorais.springbootcrud.repository.StudentRepository;
 
@@ -17,5 +27,45 @@ public class StudentResource {
 	@GetMapping("/students")
 	public List<Student> retrieveAllStudents() {
 		return studentRepository.findAll();
+	}
+	
+	@GetMapping("/students/{id}")
+	public Student retrieveStudent(@PathVariable long id) {
+		Optional<Student> student = studentRepository.findById(id);
+
+		if (!student.isPresent())
+			throw new StudentNotFoundException("id-" + id);
+
+		return student.get();
+	}
+	
+	@DeleteMapping("/students/{id}")
+	public void deleteStudent(@PathVariable long id) {
+		studentRepository.deleteById(id);
+	}
+	
+	@PostMapping("/students")
+	public ResponseEntity<Object> createStudent(@RequestBody Student student) {
+		Student savedStudent = studentRepository.save(student);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedStudent.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+	
+	@PutMapping("/students/{id}")
+	public ResponseEntity<Object> updateStudent(@RequestBody Student student, @PathVariable long id) {
+
+		Optional<Student> studentOptional = studentRepository.findById(id);
+
+		if (!studentOptional.isPresent())
+			return ResponseEntity.notFound().build();
+
+		student.setId(id);
+		
+		studentRepository.save(student);
+
+		return ResponseEntity.noContent().build();
 	}
 }
